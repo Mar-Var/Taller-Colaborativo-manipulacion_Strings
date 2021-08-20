6b8448108ebe46b8cfb37d1eb25bd79f83987146
Mar-Var <marcos.vargas@uptc.edu.co>
2021-08-19 23:13:43 -0500

    Formato inicial de aplicacion

---
+++ src/aplicacion/Features.java
@@ -0,0 +1,60 @@
+package aplicacion;
+
+public class Features {
+	
+	final String DEFAULT_MESSAGE= "Sogamoso ciudad del sol y del acero";
+	public Features() {
+		
+	}
+	public String ConvertProperName() {
+		
+		return null;
+	}
+	
+	public int searchWord(String word) {
+		
+		return 0;
+	}
+	
+	public static String encrypt(String word) {
+		
+		return null;
+	}
+	public static String decrypt(String word) {
+		
+		return null;
+	}
+	
+	public String addCharacters(char letter,int amount,char orientation) {
+		
+		return null;
+	}
+	
+	public String deleteCharacter(char letter) {
+		return null;
+	}
+	public String intersectStrings(String word) {
+
+		return null;
+	}
+	public String differentiateStrings(String word) {
+		
+		return null;
+	}
+	public String deleteCharactersWithWord(String word,char orientation) {
+		
+		return null;
+	}
+	public boolean validateEmail(String email) {
+		
+		return false;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/aplicacion/main.java
@@ -0,0 +1,11 @@
+package aplicacion;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
